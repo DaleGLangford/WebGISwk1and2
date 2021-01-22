21e092ca1e891c377bcd8d58e4e0908f155d5e19
dalelangfordfleming <dale.langford@flemingcollege.ca>
2021-01-22 10:54:58 -0500

    d

--- HelloWorldJava.js
+++ HelloWorldJava.js
@@ -4,7 +4,7 @@ function initMap() {
     // new operator creates a new instance - can be used for multiple maps
     // .Map(mapDiv:Node,Opts?:MapOptions)
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 51.178, lng: -1.826 },
+    center: { lat: 51.17896941504006, lng: -1.8261989083505485 },
     // zoom levels:
     // 1 = world
     // 5 = landmass
