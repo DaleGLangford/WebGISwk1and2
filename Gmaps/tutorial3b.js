a074a9a6a1543226049731a5c139e981bd91a8a8
dalelangfordfleming <dale.langford@flemingcollege.ca>
2021-01-23 11:56:32 -0500

    D

--- Gmaps/tutorial3b.js
+++ Gmaps/tutorial3b.js
@@ -3,7 +3,7 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 2,
-    center: new google.maps.LatLng(2.8, -187.3),
+    center: { lat: -33.865427, lng: 151.196123 },
     mapTypeId: "terrain",
   });
   // Create a <script> tag and set the USGS URL as the source.
@@ -13,17 +13,25 @@ function initMap() {
   script.src =
     "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
   document.getElementsByTagName("head")[0].appendChild(script);
+  map.data.setStyle((feature) => {
+    const magnitude = feature.getProperty("mag");
+    return {
+      icon: getCircle(magnitude),
+    };
+  });
 }
 
-// Loop through the results array and place a marker for each
-// set of coordinates.
-const eqfeed_callback = function (results) {
-  for (let i = 0; i < results.features.length; i++) {
-    const coords = results.features[i].geometry.coordinates;
-    const latLng = new google.maps.LatLng(coords[1], coords[0]);
-    new google.maps.Marker({
-      position: latLng,
-      map: map,
-    });
-  }
-};
+function getCircle(magnitude) {
+  return {
+    path: google.maps.SymbolPath.CIRCLE,
+    fillColor: "red",
+    fillOpacity: 0.2,
+    scale: Math.pow(2, magnitude) / 2,
+    strokeColor: "white",
+    strokeWeight: 0.5,
+  };
+}
+
+function eqfeed_callback(results) {
+  map.data.addGeoJson(results);
+}
--- Gmaps/tutorial3c.js
+++ Gmaps/tutorial3c.js
@@ -3,7 +3,7 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 2,
-    center: new google.maps.LatLng(2.8, -187.3),
+    center: { lat: -33.865427, lng: 151.196123 },
     mapTypeId: "terrain",
   });
   // Create a <script> tag and set the USGS URL as the source.
@@ -15,15 +15,17 @@ function initMap() {
   document.getElementsByTagName("head")[0].appendChild(script);
 }
 
-// Loop through the results array and place a marker for each
-// set of coordinates.
-const eqfeed_callback = function (results) {
+function eqfeed_callback(results) {
+  const heatmapData = [];
+
   for (let i = 0; i < results.features.length; i++) {
     const coords = results.features[i].geometry.coordinates;
     const latLng = new google.maps.LatLng(coords[1], coords[0]);
-    new google.maps.Marker({
-      position: latLng,
-      map: map,
-    });
+    heatmapData.push(latLng);
   }
-};
+  const heatmap = new google.maps.visualization.HeatmapLayer({
+    data: heatmapData,
+    dissipating: false,
+    map: map,
+  });
+}
