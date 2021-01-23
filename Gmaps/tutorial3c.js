fa65a4a5d5da28e2fd4c304a9cff9c5c75fc88ae
dalelangfordfleming <dale.langford@flemingcollege.ca>
2021-01-23 12:17:24 -0500

    D

--- Gmaps/tutorial3c.js
+++ Gmaps/tutorial3c.js
@@ -6,10 +6,10 @@ function initMap() {
     center: { lat: -33.865427, lng: 151.196123 },
     mapTypeId: "terrain",
   });
-  // Create a script tag and set the USGS URL as the source.
-  var script = document.createElement('script');
-  script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
-  document.getElementsByTagName('head')[0].appendChild(script);
+    // Create a script tag and set the USGS URL as the source.
+    var script = document.createElement('script');
+    script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
+    document.getElementsByTagName('head')[0].appendChild(script);
 
   script.src =
     "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
