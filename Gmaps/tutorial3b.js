7df8b172321c867c1c55a2b1718c07ca15a5f679
dalelangfordfleming <dale.langford@flemingcollege.ca>
2021-01-23 13:29:42 -0500

    D

--- Gmaps/tutorial3b.js
+++ Gmaps/tutorial3b.js
@@ -6,10 +6,11 @@ function initMap() {
     center: { lat: -33.865427, lng: 151.196123 },
     mapTypeId: "terrain",
   });
-  // Create a <script> tag and set the USGS URL as the source.
-  const script = document.createElement("script");
-  // This example uses a local copy of the GeoJSON stored at
-  // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+    // Create a script tag and set the USGS URL as the source.
+    var script = document.createElement('script');
+
+    script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
+    document.getElementsByTagName('head')[0].appendChild(script);
   script.src =
     "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
   document.getElementsByTagName("head")[0].appendChild(script);
