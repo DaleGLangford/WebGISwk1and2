ebfa6172dc77bf7bdf8483a0ff56b13c9497e0b6
dalelangfordfleming <dale.langford@flemingcollege.ca>
2021-01-24 10:52:11 -0500

    D

--- FinalMap/SHjava.js
+++ FinalMap/SHjava.js
@@ -27,9 +27,37 @@ function initMap() {
       imagePath:
         "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
     });
+
+///////////////////////////////////////POLYGONS///////////////////////////////////////////
+
+  // Set polygones for Avenue and Curcus
+  // Construct the avenue polygon.
+  const avenue = new google.maps.Polygon({
+    paths: avenueCoords,
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+  });
+  avenue.setMap(map);
+
+  // Construct the  cursus polygon.
+  const cursus = new google.maps.Polygon({
+    paths: cursusCoords,
+    strokeColor: "#f5aa0a",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#f5aa0a",
+    fillOpacity: 0.35,
+  });
+  cursus.setMap(map);
+
+///////////////////////////////////////CIRCLES///////////////////////////////////////////
+
 }
 
-  // Locations of Stonehenge features
+///////////////////////////////////////FEATURE LOCATIONS///////////////////////////////////////////
   const locations = [
     { lat: 51.17896941504006, lng: -1.8261989083505485 }, // Stone Henge
     { lat: 51.18047015766694, lng: -1.8231479948025724 }, // Avenue
@@ -42,46 +70,22 @@ function initMap() {
     { lat: 51.189385040727416, lng: -1.7857622285233417 }, // Wood Henge
     { lat: 51.19268362244375, lng: -1.7872467287312461 }, // Durrington Walls
   ];
+///////////////////////////////////////POLYGONS LOCATIONS///////////////////////////////////////////
 
-
-///////////////////////////////////////POLYGONS///////////////////////////////////////////
-
-// Set polygones for Avenue and Curcus
-// Define the LatLng coordinates for the polygon's path.
+  // Set polygones for Avenue and Curcus
+  // Define the LatLng coordinates for the polygon's path.
   const avenueCoords = [
     { lat: 51.179523912908344, lng: -1.8252399257461556 }, // sw corner
     { lat: 51.18256715437142, lng: -1.8195303593967194 }, // nw corner
     { lat: 51.182547160011154, lng: -1.8192113892175406 }, // ne corner
     { lat: 51.17939402446637, lng: -1.8248940019997915 }, // se corner
   ];
-  // Construct the avenue polygon.
-  const avenue = new google.maps.Polygon({
-    paths: avenueCoords,
-    strokeColor: "#FF0000",
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: "#FF0000",
-    fillOpacity: 0.35,
-  });
-  avenue.setMap(map);
 
+  const cursusCoords = [
+    { lat: 51.18483569231366, lng: -1.8356648924191814 }, // sw corner 
+    { lat: 51.18502230626078, lng: -1.8358363073614685 }, // nw corner 
+    { lat: 51.18546339077806, lng: -1.8307389681993118 }, // ne corner 
+    { lat: 51.18536725733478, lng: -1.8299315136027496 }, // se corner 
+  ];
 
-const cursusCoords = [
-  { lat: 51.18483569231366, lng: -1.8356648924191814 }, // sw corner 
-  { lat: 51.18502230626078, lng: -1.8358363073614685 }, // nw corner 
-  { lat: 51.18546339077806, lng: -1.8307389681993118 }, // ne corner 
-  { lat: 51.18536725733478, lng: -1.8299315136027496 }, // se corner 
-];
-// Construct the  cursus polygon.
-const cursus = new google.maps.Polygon({
-  paths: cursusCoords,
-  strokeColor: "#f5aa0a",
-  strokeOpacity: 0.8,
-  strokeWeight: 2,
-  fillColor: "#f5aa0a",
-  fillOpacity: 0.35,
-});
-cursus.setMap(map);
-
-
-///////////////////////////////////////CIRCLES///////////////////////////////////////////
+///////////////////////////////////////CIRCLES LOCATIONS///////////////////////////////////////////
