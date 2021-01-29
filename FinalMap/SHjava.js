54d3b1e11768ecc1fd66e3a830e0d5cd3ba58f29
dalelangfordfleming <dale.langford@flemingcollege.ca>
2021-01-29 09:58:02 -0500

    D

--- FinalMap/SHjava.js
+++ FinalMap/SHjava.js
@@ -92,7 +92,7 @@ function initMap() {
   for (const shp in woodHengeCoord) {
     // Add the circle for this city to the map.
     const woodHenge = new google.maps.Circle({
-      strokeColor: "#35ff03",
+      strokeColor: "#f8fc03",
       strokeOpacity: 0.8,
       strokeWeight: 2,
       // fillColor: "#FF0000",
