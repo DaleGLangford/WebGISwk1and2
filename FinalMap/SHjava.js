1d31a252449413239280b380b1e287a8ae1191e1
dalelangfordfleming <dale.langford@flemingcollege.ca>
2021-01-29 10:34:35 -0500

    D

--- FinalMap/SHjava.js
+++ FinalMap/SHjava.js
@@ -107,7 +107,7 @@ function initMap() {
   for (const shp in durringtonCoord) {
     // Add the circle for this city to the map.
     const durrington = new google.maps.Circle({
-      strokeColor: "##fcba05",
+      strokeColor: "#02faf6",
       strokeOpacity: 0.8,
       strokeWeight: 2,
       // fillColor: "#FF0000",
